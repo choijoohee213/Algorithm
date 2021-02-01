51690aedcd2d9deaf6d13f3bb4cd10f0db30416d
choijoohee213 <choijoohee@naver.com>
2021-02-01 19:38:50 +0900

    Update 21/02/01 프로그래머스_level2_땅따먹기

--- Programmers/Level2/땅따먹기_dp.cpp
+++ Programmers/Level2/땅따먹기_dp.cpp
@@ -3,20 +3,20 @@ using namespace std;
 
 int solution(vector<vector<int> > land)
 {
-    int answer = 0, maxValue = 0, r = land.size()-2 == 0 ? 1 : land.size()-2;
-    
-    while(r>=0){
-        for(int i=0; i<4; i++){
-            for(int j=0; j<4; j++){
-                if(i!=j && maxValue<land[r+1][j]) maxValue = land[r+1][j];
+    int answer = 0, maxValue = 0;
+    for(int i=land.size()-2; i>=0; i--){
+        for(int j=0; j<4; j++){
+            for(int k=0; k<4; k++){
+                if(j!=k && maxValue < land[i+1][k])
+                    maxValue = land[i+1][k];
             }
-            land[r][i] += maxValue;
+            land[i][j] += maxValue;
             maxValue = 0;
         }
-        r--;
     }
     
     for(int i=0; i<4; i++)
         if(maxValue < land[0][i]) maxValue = land[0][i];
+    
     return maxValue;
 }
