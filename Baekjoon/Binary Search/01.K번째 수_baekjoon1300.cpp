bb89a143077a5c466a3e0d41a235490292d2b5aa
DESKTOP-QQ11LUI\choijoohee <choijoohee@naver.com>
2021-08-18 15:13:47 +0900

    21/08/18 백준_K번째 수(1300)

--- Baekjoon/Binary Search/01.K번째 수_baekjoon1300.cpp
+++ Baekjoon/Binary Search/01.K번째 수_baekjoon1300.cpp
@@ -2,17 +2,21 @@
 using namespace std;
 
 int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+  
   long long n,k;
   cin>>n>>k;
   
-  long long f = 1, e = n*n, result;
+  long long f = 1, e = n*n, result, m, sum;
+  
   while(f<=e){
-    long long m = (f+e) / 2;
-    long long sum = 0;
-    for(long long i = 1; i<n+1; i++){
-      sum += min(n, m / i);]
-    }
-    if(sum<k) f = m -1;
+    m = (f+e) / 2;
+    sum = 0;
+    for(long long i = 1; i<n+1; i++)
+      sum += min(n, m / i);  //mid보다 작은 j의 수(i * j <= mid)
+    if(sum<k) 
+      f = m + 1;
     else {
       result = m;
       e = m -1;
