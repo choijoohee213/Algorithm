6fea2cca1e8030d47a72b1f5f7029f6d9c466063
DESKTOP-QQ11LUI\choijoohee <choijoohee@naver.com>
2021-10-24 19:34:19 +0900

    21/10/24 백준_귀여운 수(17294)

---
+++ Baekjoon/implementation/03.귀여운 수_baekjoon17294.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    int diff;
+    bool result = true;
+
+    for(int i=1; i<s.size(); i++){
+        int x = s[i] - '0';
+        int prev = s[i-1]-'0';
+        if(i == 1){
+            diff = prev - x;
+            continue;
+        }
+        if(prev - x != diff){
+            result = false;
+            break;
+        }
+    }
+    if(result) cout<<"?(?????)?..¡Æ¢½ ²î¿ä¹Ì!!";
+    else cout<<"ÈïÄ©»×!! <(£þ ? £þ)>";
+    º¸¼ºÀÌ ¹«½¼ÁË
+}
