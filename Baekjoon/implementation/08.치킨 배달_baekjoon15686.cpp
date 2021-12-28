0f522c899b9d43c851abecf3d9760be98a26c90a
DESKTOP-QQ11LUI\choijoohee <choijoohee@naver.com>
2021-12-28 22:06:10 +0900

    21/12/28 백준_치킨 배달(15686)

--- Baekjoon/implementation/08.치킨 배달_baekjoon15686.cpp
+++ Baekjoon/implementation/08.치킨 배달_baekjoon15686.cpp
@@ -2,26 +2,20 @@
 using namespace std;
 typedef pair<int,int> p;
 
-int n,m, result_min = INT_MAX;
+int n,m;
+long long result_min = LONG_LONG_MAX;
 vector<p> home;
 vector<p> chicken;
 vector<int> result;
-
-void dfs(int index, int cnt) {
-    if(cnt >= m) return;
-    result.push_back(index);
-    for(int i=index; i<chicken.size(); i++) {
-        dfs(i, cnt+1);
-    }
-}
+bool selected[15] = {false,};
 
 void calDistance() {
-    int sum = 0;
+    long long sum = 0;
 
     for(int i=0; i<home.size(); i++) {
         int x = home[i].first;
         int y = home[i].second;
-        int minDistance = n*n*n;
+        int minDistance = INT_MAX;
 
         for(int j=0; j<result.size(); j++) {
             minDistance = min(minDistance, (abs)(chicken[result[j]].first - x) + (abs)(chicken[result[j]].second - y));
@@ -29,6 +23,21 @@ void calDistance() {
         sum += minDistance;
     }
     result_min = min(result_min, sum);
+}
+
+void dfs(int index, int cnt) {
+    if(cnt >= m) {
+        calDistance();
+        return;
+    }
+    for(int i=index; i<chicken.size(); i++) {
+        if(selected[i]) continue;
+        selected[i] = true;
+        result.push_back(i);
+        dfs(i, cnt+1);
+        selected[i] = false;
+        result.pop_back();
+    }
 }
 
 int main() {
@@ -49,11 +58,6 @@ int main() {
             }
         }
     }
-
-    for(int i=0; i<chicken.size(); i++) {
-        result.clear();
-        dfs(i, 0);
-        calDistance();
-    }
+    dfs(0, 0);
     cout<<result_min;
 }
