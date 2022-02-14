9065ad523cfed6d89295e743767796c4cbdf1f02
choijoohee213 <choijoohee@naver.com>
2022-02-14 17:54:31 +0900

    22/02/14 백준_일곱난쟁이 수정(2309)

--- Baekjoon/BruteForce/09.일곱 난쟁이_baekjoon2309.java
+++ Baekjoon/BruteForce/09.일곱 난쟁이_baekjoon2309.java
@@ -10,19 +10,19 @@ class Main {
 		for (int i=0; i<9; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
-		cal(0, 0);
+		cal(0, 0, 0);
 		Arrays.sort(result);
 		for(int i=0; i<7; i++) {
 			System.out.println(result[i]);
 		}
 	}
 
-	static boolean cal(int cnt, int sum) {
+	static boolean cal(int cnt, int idx, int sum) {
 		if(cnt == 7 && sum == 100) {
 			return true;
 		}
-		for(int i=cnt; i<9; i++) {
-			if(cal(cnt + 1,sum + arr[i])) {
+		for(int i=idx; i<9; i++) {
+			if(cal(cnt + 1,i+1, sum + arr[i])) {
 				result[cnt] = arr[i];
 				return true;
 			}
